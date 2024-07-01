66c385e58b870f25980848e0d3fe7ef0076301c1
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2024-07-01 23:35:06 +0530

    Add google authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -36,3 +36,30 @@ export const signin = async(req, res, next)=>{
         next(error);
     }
 }
+
+export const google=async(req, res, next)=>{
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if(user){
+            //Sign in
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+            const{password: pass, ...resData} = user._doc;
+            res.cookie("access_token", token, {httpOnly: true}).status(200).json(resData);
+        }else{
+            //Sign up
+            //Generate a 16 character random password
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
+
+            const newUser = new User({username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), 
+                email: req.body.email, password: hashedPassword, avatar: req.body.profileImage});
+            await newUser.save();
+
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+            const{password: pass, ...resData} = newUser._doc;
+            res.cookie("access_token", token, {httpOnly: true}).status(200).json(resData);
+        }
+    } catch (error) {
+        
+    }
+}
