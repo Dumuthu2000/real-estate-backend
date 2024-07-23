6c8c9aa7f259848dcfc9e60b535957dc10f01771
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2024-07-23 22:45:10 +0530

    Update create listing controller

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -2,7 +2,7 @@ import Listing from "../model/listing.model.js";
 
 export const createListing=async(req, res, next)=>{
     try {
-        const listing = Listing.create(req.body);
+        const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
     } catch (error) {
         next(error);
