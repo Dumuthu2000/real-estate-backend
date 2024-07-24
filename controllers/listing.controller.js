4cbc1c3946beec42cdeeaf7c0680c7d13e10aac4
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2024-07-24 14:07:28 +0530

    Add user listing delete API route

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -1,9 +1,30 @@
 import Listing from "../model/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing=async(req, res, next)=>{
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
+    } catch (error) {
+        next(error);
+    }
+}
+
+//Delete a listing
+ export const deleteListing = async(req, res, next)=>{
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'Listing is not found'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only delete your own listings!!!!'));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing deleted successfully!!!');
     } catch (error) {
         next(error);
     }
