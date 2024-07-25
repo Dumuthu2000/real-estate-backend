8119655a476793b2908e1a5e7282049bfcd7cf58
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2024-07-25 20:49:47 +0530

    Create user details fetching API route

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { updateUser, deleteUser, getUserListings } from '../controllers/user.controller.js';
+import { updateUser, deleteUser, getUserListings, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 const router = express.Router();
 
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
