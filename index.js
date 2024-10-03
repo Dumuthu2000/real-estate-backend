5d03a4ee3244374c079390982023331b9d82e6d0
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-10-03 11:59:15 +0530

    update port

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ app.use(cors({
     credentials: true
   }))
 
-app.listen(5000,()=>{
-    console.log(`Server is running on port ${5000}`)
+const PORT = process.env.PORT
+app.listen(PORT || 5000,()=>{
+    console.log(`Server is running on port ${PORT}`)
 });
 
 app.use('/api/user', userRouter)
